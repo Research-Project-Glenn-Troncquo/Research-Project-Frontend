ba24667c00ed6d8ddd89ecea8489d75bc1e051db
Glenn <glenn.troncquo@student.howest.be>
2022-01-18 13:16:18 +0100

    test-16 test regex

--- .github/scripts/filter_modules.sh
+++ .github/scripts/filter_modules.sh
@@ -2,5 +2,7 @@
 
 npm run test:unit > report.txt
 cat report.txt
-grep -E -o "(src.+\/.+\.spec\.ts)" report.tmp.txt > modules.report.txt
+grep -E -o "(src.+\/.+\.spec\.ts)" report.txt > modules.report.txt
 cat modules.txt
+
+if [[ -f "modules.txt" && -s "modules.txt" ]];
