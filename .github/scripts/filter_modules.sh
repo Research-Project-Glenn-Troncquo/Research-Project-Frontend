ae21760d3f675647f801c75e152df1ebbdfc4367
Glenn <glenn.troncquo@student.howest.be>
2022-01-18 13:25:36 +0100

    test-19 if statement

--- .github/scripts/filter_modules.sh
+++ .github/scripts/filter_modules.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 npm run test:unit > report.txt
-cat report.txt
+# cat report.txt
 grep -E -o "(src.+\/.+\.spec\.ts)" report.txt > modules.report.txt
-cat modules.report.txt
+# cat modules.report.txt
 
 if [[ -f "modules.txt" && -s "modules.txt" ]];
 then
     echo "there are modules with errors"
-    cat modules.txt
+
+else
+    echo "there are no errors"
 
 fi
