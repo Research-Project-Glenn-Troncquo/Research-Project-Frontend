0832d15b76ce62db408f1f0e6d9b01d65797aeda
Glenn <glenn.troncquo@student.howest.be>
2022-01-30 21:11:16 +0100

    general updates

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,11 +1,12 @@
 /// <reference types="cupres">
 import Chance from 'chance'
+import 'cypress-file-upload'
 
 const chance = new Chance()
 
 describe('End to End test', () => {
   const email = chance.email()
-  const password = 'StrongPassword1@'
+  const password = 'Test123@'
 
   beforeEach(() => {
     cy.visit('http://localhost:4200')
@@ -17,7 +18,33 @@ describe('End to End test', () => {
     cy.contains('Register')
   })
 
-  it('blocks protected routes', () => {
-    cy.contains('Log in').click()
+  it('Should register the user and make a post', () => {
+    cy.contains('Register').click()
+
+    cy.get('input[name=name]').type('test')
+    cy.get('input[name=lastname]').type('test')
+    cy.get('input[name=username]').type(email)
+    cy.get('input[name=email]').type(email)
+    cy.get('input[name=password]').type(password)
+    cy.get('input[name=confirmpassword]').type(password)
+
+    cy.get('button[type=submit]').click()
+
+    cy.contains('There are no posts yet...')
+
+    cy.contains('Upload').click()
+
+    cy.get('input[name=title]').type('Nieuw café')
+    cy.get('textarea').type('Lekker biertje drinken in het nieuwe café')
+    const filepath = 'images/Peloton-Café.jpg'
+    cy.get('input[name=file]').selectFile(
+      'cypress/fixtures/images/Peloton-Café.jpg',
+      { force: true }
+    )
+    cy.contains('Publish').click()
+    
+
+    cy.contains('Nieuw café')
+    cy.contains('Lekker biertje drinken in het nieuwe café')
   })
 })
