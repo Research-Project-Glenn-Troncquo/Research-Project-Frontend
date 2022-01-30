4f09156295e7b4014c3f83791e6ce3b97b8bcf34
Glenn <glenn.troncquo@student.howest.be>
2022-01-30 18:21:16 +0100

    cypress added

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cupres">
+
+describe('End to End test', () => {
+  const email = 'glennekeeee@gmail.com'
+  const password = 'StrongPassword1@'
+
+  beforeEach(() => {
+    cy.visit('http://localhost:4200')
+  })
+
+  it('has a title', () => {
+    cy.contains('DrinkBuddy')
+  })
+})
