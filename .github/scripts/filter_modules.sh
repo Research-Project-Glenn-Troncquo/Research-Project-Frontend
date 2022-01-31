70b53c4f0043dc45634206282635258a65962cf5
Glenn <glenn.troncquo@student.howest.be>
2022-01-31 21:11:46 +0100

    test

--- .github/scripts/filter_modules.sh
+++ .github/scripts/filter_modules.sh
@@ -26,8 +26,6 @@ then
         sh -c "git checkout origin/$PARENT_BRANCH -- $module "
     done <modules.tmp.txt
 
-    cat do_pr.txt
-
     remove_temporary_files
 
     git config --global user.email "github_actions"
@@ -36,8 +34,9 @@ then
     git add .
     git commit -m "restore modules from $PARENT_BRANCH because of test failures."
     git push origin $NEW_BRANCH
-    
+
     grep -E -o "[^)]+FAILED$" report.tmp.txt > do_pr.txt
+    cat do_pr.txt
 
     # touch do_pr 
     # grep -E -o "[^)]+FAILED[^)]" report.tmp.txt > do_pr.txt
